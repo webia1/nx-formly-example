8586a845cabd78fd3b72a0099796ffe3a2eb0a45
Erkan Buelbuel <erkan.buelbuel@bit.admin.ch>
2021-06-01 14:26:13 +0200

    Formly Installed

--- apps/nx-formly/src/app/app.module.ts
+++ apps/nx-formly/src/app/app.module.ts
@@ -3,10 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormlyModule } from '@ngx-formly/core';
+import { FormlyMaterialModule } from '@ngx-formly/material';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FormlyModule.forRoot({ extras: { lazyRender: true } }),
+    FormlyMaterialModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
